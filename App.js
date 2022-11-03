8ac2928c94c0791f4817b1efbb9d42070f66291c
dagp-ucol <dgentner@ucol.mx>
2022-11-02 21:33:48 -0600

    Add Text Alert

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { Component } from 'react';
-import { StyleSheet, Text, View, Button, Switch } from 'react-native';
+import { StyleSheet, Text, View, Button, Switch, SafeAreaView, TextInput, onChangeText, Alert } from 'react-native';
 // import { Switch } from 'react-native-web';
 
 export default class App extends Component<Props> {
@@ -8,25 +8,42 @@ export default class App extends Component<Props> {
     super(props);
     this.state = {
       switchValue: false,
+      newText: '',
     };
-  }
+  };
+
   onChange = (value) => {
     console.warn("Switch value: ", value);
     this.setState({ switchValue: value });
   };
+
+  onChangeText = (text) => {
+    console.warn(`El valor cambiará a: ${text}`);
+    this.setState({newText: text})
+  };
+
   onPressLearnMore = () => {
     console.log('onPressLearnMore');
-  }
+  };
+
+  onPressAlert = (text) => {
+    alert(this.state.newText);
+  };
+
   render() {
     return (
       <View style={styles.container}>
+      <TextInput
+          style={styles.input}
+          onChangeText={this.onChangeText}
+      />
       <Switch
         onValueChange = {() => this.onChange(!this.state.switchValue)}
         value = {this.state.switchValue}
       />
       <Button
-        onPress={this.onPressLearnMore}
-        title="Learn More"
+        onPress={this.onPressAlert}
+        title="Alert"
         color="#841584"
         accessibilityLabel="Learn more about this purple button"
       />
@@ -41,5 +58,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: .5,
+    padding: 10,
+    width: 200,
   },
 });
