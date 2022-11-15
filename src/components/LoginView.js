c1a08704886e979ae322c610f75ad15c3717b61f
dagp-ucol <dgentner@ucol.mx>
2022-11-15 08:25:34 -0600

    Fix styles

--- src/components/LoginView.js
+++ src/components/LoginView.js
@@ -15,24 +15,39 @@ export default class LoginView extends Component {
         Actions.home();
     }
 
+    validateInputs = () => {
+        // Validate email and password
+        // If is blank
+        if (this.state.email == "" || this.state.password == "") {
+            Alert.alert("Error", "Email and password are required");
+            return;
+        }
+
+        // If email is valid regex
+        if (!this.state.email.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i)) {
+            Alert.alert("Error", "Email is not valid");
+            return;
+        }
+
+        // Validate password 8 characters, 1 number, 1 uppercase, 1 lowercase
+        if (!this.state.password.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/)) {
+            Alert.alert("Error", "Password is not valid, must be 8 characters, 1 number, 1 uppercase, 1 lowercase");
+        } else {
+            this.getLogin();
+        }
+    }
+
     render() {
         return (
         <View style={styles.container}>
-            {/* <Text>Hola</Text>
-            <Button
-                title="Login"
-                style={styles.button}
-                onPress={() => {
-                    this.getLogin();
-                    Alert.alert("Hola mundo", "Router esta funcionando")
-            }}
-            /> */}
             {/* Set App logo image here */}
             <Image
                 style={styles.logo}
                 source={require("../assets/images/logo.png")}
             />
 
+            {/* Title in big font and bold */}
+            <Text style={styles.title}>Spookyfy</Text>
 
             {/* Login Inputs */}
             <TextInput
@@ -51,28 +66,7 @@ export default class LoginView extends Component {
             {/* Login Button */}
             <Button
                 title="Login"
-                onPress={() => {
-                    // Validate email and password
-                    // If is blank
-                    if (this.state.email == "" || this.state.password == "") {
-                        Alert.alert("Error", "Email and password are required");
-                        return;
-                    }
-
-                    // If email is valid regex
-                    if (!this.state.email.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i)) {
-                        Alert.alert("Error", "Email is not valid");
-                        return;
-                    }
-
-                    // Validate password 8 characters, 1 number, 1 uppercase, 1 lowercase
-                    if (!this.state.password.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/)) {
-                        Alert.alert("Error", "Password is not valid, must be 8 characters, 1 number, 1 uppercase, 1 lowercase");
-                    } else {
-                        this.getLogin();
-                    }
-
-            }}
+                onPress={this.validateInputs}
             />
         </View>
         );
@@ -93,6 +87,8 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderColor: "black",
         marginBottom: 10,
+        // roundenBorders
+        borderRadius: 10,
     },
     password: {
         width: 200,
@@ -102,6 +98,7 @@ const styles = StyleSheet.create({
         borderColor: "black",
         marginBottom: 10,
         // passwordDisguised
+        borderRadius: 10,
     },
     button: {
         width: 200,
@@ -110,5 +107,16 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderColor: "black",
         marginBottom: 10,
+        borderRadius: 10
+    },
+    title: {
+        fontSize: 30,
+        fontWeight: "bold",
+        marginBottom: 20,
+    },
+    logo: {
+        width: 200,
+        height: 200,
+        marginBottom: 0,
     },
 });
