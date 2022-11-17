86ce7a8f60e8f22190807fcf24c685c44a711c82
dagp-ucol <dgentner@ucol.mx>
2022-11-17 08:28:25 -0600

    Back button working

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import LoginView from './src/components/LoginView';
 import HomeView from './src/components/HomeView';
 import { Actions, Router, Scene } from 'react-native-router-flux';
-// import { Switch } from 'react-native-web';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const scenes = Actions.create(
   <Scene key="root">
-    <Scene key="login" component={LoginView} title="Login" initial={true} hideNavBar />
-    <Scene key="home" component={HomeView} title="Home" />
+    <Scene key="login" component={LoginView} title="Login" initial={true}  hideNavBar />
+    <Scene key="home" component={HomeView} title="Home"
+      onLeft={() => {Actions.pop()}} back = {true}
+    />
   </Scene>
 );
 
@@ -16,3 +18,11 @@ export default class App extends Component {
     return <Router scenes={scenes} />;
   }
 };
+
+const renderCustomButton = () => {
+  return () => (
+    <TouchableOpacity>
+      <Icon name="back" size={30} color="#900" />
+    </TouchableOpacity>
+  );
+}
